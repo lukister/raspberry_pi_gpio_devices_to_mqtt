0109b896a6b8601c6fd43ce4265d498195d79ca8
lmarkowski <lmarkowski@psmm.pl>
2024-02-13 20:58:31 +0100

    Test connection with mqtt broker

---
+++ src/RaspberryPiGpioToMqtt.App/MqttClient.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using MQTTnet.Client;
+using MQTTnet;
+
+public sealed class MqttClient : IAsyncDisposable
+{
+    private readonly IConfiguration _configuration;
+    private IMqttClient _mqttClient;
+
+    public MqttClient(IConfiguration configuration)
+    {
+        var mqttFactory = new MqttFactory();
+        _mqttClient = mqttFactory.CreateMqttClient();
+        _configuration = configuration;
+    }
+
+    private async Task ConnectIfDisconected()
+    {
+        if (_mqttClient == null || _mqttClient.IsConnected)
+            return;
+        var mqttClientOptions = new MqttClientOptionsBuilder()
+        .WithTcpServer(_configuration["Server"]!)
+        .WithCredentials(_configuration["User"]!, _configuration["Password"]!)
+        .Build();
+
+        await _mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+    }
+
+    public async Task SendData(string topic, string payload)
+    {
+        await ConnectIfDisconected();
+        var message = new MqttApplicationMessageBuilder()
+         .WithTopic(topic)
+         .WithPayload(payload)
+         .Build();
+        await _mqttClient.PublishAsync(message);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        var disconectOptions = new MqttClientDisconnectOptionsBuilder()
+            .WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection)
+            .Build();
+        await _mqttClient.DisconnectAsync(disconectOptions);
+        _mqttClient.Dispose();
+    }
+}
---
+++ src/RaspberryPiGpioToMqtt.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Globalization;
+
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var client = new MqttClient(configuration.GetSection("Mqtt"));
+
+try
+{
+    var random = new Random();
+    var temperature = (random.Next(150, 220) / 10d).ToString(CultureInfo.InvariantCulture);
+    await client.SendData("greenhouse/in/temperature", temperature);
+    var humidity = random.Next(30, 60).ToString();
+    await client.SendData("greenhouse/in/humidity", humidity);
+}
+finally
+{
+
+    await client.DisposeAsync();
+}
