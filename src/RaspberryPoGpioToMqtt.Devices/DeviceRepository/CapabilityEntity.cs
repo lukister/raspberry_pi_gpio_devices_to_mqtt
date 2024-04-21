b7a8f3b619077ee764a4d902e96b29b2a7e99c35
lmarkowski <lmarkowski@psmm.pl>
2024-04-21 23:33:31 +0200

    Fix getting options

--- src/RaspberryPoGpioToMqtt.Devices/DeviceRepository/CapabilityEntity.cs
+++ src/RaspberryPoGpioToMqtt.Devices/DeviceRepository/CapabilityEntity.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace RaspberryPoGpioToMqtt.Devices.DeviceRepository;
 
@@ -8,16 +9,13 @@ internal class CapabilityEntity
     public string Type { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     [JsonExtensionData]
-    public Dictionary<string, object> AdditionalConfiguration { get; set; } = [];
+    public Dictionary<string, JsonElement> AdditionalConfiguration { get; set; } = [];
 
     public T GetValue<T>(string name)
     {
         if (!AdditionalConfiguration.TryGetValue(name, out var value))
-            throw new Exception();
-
-        if (value is T)
-            return (T)value;
-
-        return (T)Convert.ChangeType(value, typeof(T));
+            throw new Exception($"{name} is missing in configuration");
+        return value.Deserialize<T>() 
+            ?? throw new Exception($"Unable to get value: {name} from {value}");
     }
 }
