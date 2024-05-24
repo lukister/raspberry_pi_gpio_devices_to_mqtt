1740f40f5a1e187dd813f610dfdfa799bc0b49ff
lmarkowski <lmarkowski@psmm.pl>
2024-05-24 19:25:26 +0200

    Add open telemetry exporter

--- src/RaspberryPiGpioToMqtt.App/Program.cs
+++ src/RaspberryPiGpioToMqtt.App/Program.cs
@@ -1,10 +1,34 @@
-﻿using RaspberryPiGpioToMqtt.App.MQTT;
+﻿using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using RaspberryPiGpioToMqtt.App.MQTT;
 using RaspberryPoGpioToMqtt.Devices;
 
 var builder = WebApplication.CreateBuilder();
 builder.Configuration.AddJsonFile("appsettings.json", optional: false);
 builder.Configuration.AddUserSecrets<Program>();
 builder.Configuration.AddEnvironmentVariables();
+
+var serviceName = nameof(RaspberryPiGpioToMqtt);
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName))
+        .AddOtlpExporter();
+});
+
+builder.Services.AddOpenTelemetry()
+      .ConfigureResource(resource => resource.AddService(serviceName))
+      .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
+          .AddOtlpExporter())
+      .WithMetrics(metrics => metrics
+          .AddAspNetCoreInstrumentation()
+          .AddOtlpExporter());
 
 builder.Services.AddOptions<SensorsOptions>()
     .Bind(builder.Configuration.GetSection(SensorsOptions.SectionName))
